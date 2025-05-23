0c3b59931dd87fe29d71ec476855314a905d86ba
Vivek Kulkarni <Vivek.kulkarni@hansencx.com>
2025-05-23 19:48:03 +0530

    New automation project added

---
+++ console-log.js
@@ -0,0 +1,5 @@
+console.log("Hello, world!"); // Prints "Hello, world!" to the console
+let age = 30;
+console.log("Age:", age); // Prints "Age: 30" to the console
+let person = { name: "John", city: "New York" };
+console.log("Person:", person);
