35c277e999d747d1046f8d34a6641c1994505ff1
Vivek <vkvk783@gmail.com>
2026-01-23 09:15:49 +0530

    files removed from uat branch

--- console-log.js
+++
@@ -1,5 +0,0 @@
-console.log("Hello, world!"); // Prints "Hello, world!" to the console
-let age = 30;
-console.log("Age:", age); // Prints "Age: 30" to the console
-let person = { name: "John", city: "New York" };
-console.log("Person:", person);
